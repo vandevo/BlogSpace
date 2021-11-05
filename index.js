09e4131308f5446158687a8b3b9d03543cbf7c7e
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-11-05 15:49:38 +0000

    master

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-fetch('https://apis.scrimba.com/jsonplaceholder/posts')
-  .then(res => res.json())
-  .then(data => {
-    const postArr = data.slice(0, 5)
-    let html = ""
-    for (let post of postArr){
-        html += `
-        <h3>${post.title}</h3>
-        <p>${post.body}</p>
-        </hr>
-        `
-    document.getElementById('blogPost').innerHTML = html
+/**
+ Challenge:
+
+ Style it up!
+ 
+ * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
+ * Add a font from Google Fonts.
+ * Any other styling you want to make it look nice!
+ 
+ */
+
+ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+ .then(res => res.json())
+ .then(data => {
+     const postsArr = data.slice(0, 5)
+     let html = ""
+     for (let post of postsArr) {
+         html += `
+             <h3>${post.title}</h3>
+             <p>${post.body}</p>
+             <hr />
+         `
+     }
+     document.getElementById("blog-list").innerHTML = html
+ })
+
+ document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
     }
-    
-    
-  
-  
-  
-});
+    console.log(data)
+})
+
+fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
+    method: "POST",
+    body: JSON.stringify({
+        title: "Buy Milk",
+        complete: false
+    })
+})
+    .then(res => res.json())
+    .then(data => con)
