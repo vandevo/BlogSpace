6795ea97a706df79b8e382a3e4ea6b3886ab3bb9
Van Vo <hello@vandevo.com>
2021-11-15 22:08:19 +0000

    master

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-/**
- Challenge:
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
 
- Style it up!
- 
- * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
- * Add a font from Google Fonts.
- * Any other styling you want to make it look nice!
- 
- */
-
- fetch("https://apis.scrimba.com/jsonplaceholder/posts")
- .then(res => res.json())
- .then(data => {
-     const postsArr = data.slice(0, 5)
-     let html = ""
-     for (let post of postsArr) {
-         html += `
-             <h3>${post.title}</h3>
-             <p>${post.body}</p>
-             <hr />
-         `
-     }
-     document.getElementById("blog-list").innerHTML = html
- })
-
- document.getElementById("new-post").addEventListener("submit", function(e) {
+document.getElementById("new-post").addEventListener("submit", function(e) {
     e.preventDefault()
     const postTitle = document.getElementById("post-title").value
     const postBody = document.getElementById("post-body").value
@@ -43,6 +32,15 @@
     
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(post => {
+            /**
+             * Challenge: Update the DOM with the new blog entry
+             */
+            document.getElementById("blog-list").innerHTML = `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("blog-list").innerHTML}
+            `
+        })
 })
-
